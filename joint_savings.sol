f72fb61c2cf23f81400765f00b64f5cd41882b1b
Haywar Zhu <haywar@Haywars-MacBook-Air.local>
2023-10-03 21:03:14 -0400

    finished function

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,8 +1,11 @@
+
 /*
 Joint Savings Account
 ---------------------
 
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses 
+that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements 
+from the financial institution to provide the features of the joint savings account.
 
 The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
 You will do the following:
@@ -27,6 +30,11 @@ contract JointSavings {
     - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
     */
     // YOUR CODE HERE!
+    address payable public accountOne;
+    address payable public accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
 
     /*
     Define a function named **withdraw** that will accept two arguments.
@@ -36,28 +44,45 @@ contract JointSavings {
     function withdraw(uint amount, address payable recipient) public {
 
         /*
-        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. 
+        The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
         // YOUR CODE HERE!
+        require(
+            recipient == accountOne || recipient == accountTwo,
+            "You don't own this account!"
+        );        
 
         /*
-        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. 
+        If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
         // YOUR CODE HERE!
+        require(
+            amount <= contractBalance,
+            "Insufficient funds!"
+        );
 
         /*
-        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
+        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, 
+        then set it to the current value of `recipient`.
         */
         // YOUR CODE HERE!
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
         // YOUR CODE HERE!
+        recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
         // YOUR CODE HERE!
+        lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
         // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
@@ -67,6 +92,7 @@ contract JointSavings {
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
         // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -76,10 +102,15 @@ contract JointSavings {
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
         // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
     // YOUR CODE HERE!
+    function() external payable { 
+        deposit();
+    }
 }
